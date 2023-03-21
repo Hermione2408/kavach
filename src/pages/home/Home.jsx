924cd38ce8fcdb3c9aa49d5d9e8b7bf69cdb565a
Hermione Dadheech <hermionedadheech@gmail.com>
2023-03-21 08:16:35 +0530

    Debugging.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,8 @@ import { UserContext } from "../../context/userContext";
 import AccidentReportModal from "../../components/AccidentReport";
 import { useNavigate, useLocation} from "react-router-dom";
 import HomeMap from "../../components/HomeMap";
+import localForage from "localforage";
+
 const Home = () => {
   const { userDetails,showLogin } = useContext(UserContext);
   const [searchTerm, setSearchTerm] = useState('');
@@ -12,21 +14,21 @@ const Home = () => {
   const location = useLocation();
   useEffect(() => {
     let from = location.state?.from?.pathname;
-    let isReportVisited = window.localStorage.getItem('reportAccidentVisited')
-    if(!isReportVisited){
-      navigate('/reportAccident')
-    }
-    return (()=>{
-      window.localStorage.clear()
-    })
+    localForage.getItem("reportAccidentVisited").then(value => {
+      if (!value) {
+        navigate("/reportAccident");
+      }
+    });
+    return () => {
+      localForage.clear();
+    };
   }, []);
   return (
     <>
-    <div className='map-container'>
-      <HomeMap />
-    </div>
+      <div className="map-container">
+        <HomeMap />
+      </div>
     </>
-    
   );
 };
 
